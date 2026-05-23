68bc996e0660a887cf2e06c28ed820d6c1a5e24e
Violet Grace <lingblack87661@gmail.com>
2026-05-22 22:41:02 -0700

    Finished implementing video converter using ffmpeg wasm

---
+++ client/src/utils/videoConverter.js
@@ -0,0 +1,30 @@
+/**
+ * @author Wai Lok Daniel Tam
+ * @description A utility function to convert a video file to a mp3 file.
+ */
+
+// Imports
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { fetchFile, toBlobURL } from '@ffmpeg/util';
+
+/**
+ * Converts a video file to a mp3 file.
+ * @param {File} videoFile - The video file to convert.
+ * @returns {Promise<string>} A promise that resolves to the converted mp3 file.
+ */
+export async function videoToMp3(videoFile) {
+    const ffmpeg = new FFmpeg();
+    await ffmpeg.load();
+
+    const fileType = videoFile.name.split('.').pop();
+    await ffmpeg.writeFile(`input.${fileType}`, await fetchFile(videoFile));
+    await ffmpeg.exec([
+        '-i', `input.${fileType}`,
+        '-q:a', '0', // Set the audio quality (codec-specific, VBR). This is an alias for -q:a.
+        '-map', 'a', // ’a’ for audio
+        'output.mp3'
+    ]);
+
+    const data = await ffmpeg.readFile('output.mp3');
+    return new Blob([data.buffer], { type: 'audio/mpeg' });
+}
